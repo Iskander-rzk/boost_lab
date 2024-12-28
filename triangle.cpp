a5cc612662773442813bda4ae2e67055ac399e28
Iskander-rzk <sasha1koval@gmail.com>
2024-12-28 12:01:40 +0700

    release 2

--- triangle.cpp
+++ triangle.cpp
@@ -3,20 +3,21 @@
 #include <boost/math/constants/constants.hpp>
 #include <boost/math/special_functions/sqrt1pm1.hpp>
 
-using namespace std;
-
 double calculate_area_by_sides(double a, double b, double c) {
     // Полупериметр
     double s = (a + b + c) / 2.0;
+    if((s <= a) or (s <= b) or (s <= c))
+    {
+        return -1;
+    }
     
-    // Формула Герона: A = sqrt(s * (s - a) * (s - b) * (s - c))
-    double area = boost::math::sqrt1pm1(s * (s - a) * (s - b) * (s - c));
+    double area = boost::math::sqrt1pm1(s * (s - a) * (s - b) * (s - c));   // Формула Герона
     return area;
 }
 
 
 double calculate_area_by_base_and_height(double base, double height) {
-    return boost::math::sqrt1pm1(base * height);
+    return 0.5 * base * height;
 }
 
 double calculate_perimeter(double a, double b, double c) {
@@ -24,17 +25,35 @@ double calculate_perimeter(double a, double b, double c) {
 }
 
 int main() {
-    // Пример 1: Если известны три стороны
-    double a = 3, b = 4, c = 5;
-    double perimeter = calculate_perimeter(a, b, c);
-    double area_by_sides = calculate_area_by_sides(a, b, c);
-    cout << "Периметр: " << perimeter << endl;
-    cout << "Площадь (по сторонам): " << area_by_sides << endl;
-
-    // Пример 2: Если известно основание и высота
-    double base = 5, height = 4;
-    double area_by_base_and_height = calculate_area_by_base_and_height(base, height);
-    cout << "Площадь (по основанию и высоте): " << area_by_base_and_height << endl;
-
+    std::cout  << "0. Search triangle by sides" << std::endl << "1. Search triangle by base and height" << std::endl;
+    int n;
+    double a, b, c, h;
+    std::cin >> n;
+    if(n == 0)
+    {
+        // Пример 1: Если известны три стороны
+        std::cout << "Enter sides" << std::endl;
+        std::cin >> a >> b >> c;
+        double perimeter = calculate_perimeter(a, b, c);
+        double area_by_sides = calculate_area_by_sides(a, b, c);
+        std::cout << "Perimeter: " << perimeter << std::endl;
+        if(area_by_sides == -1)
+        {
+            std::cout << "this is not triangle" << std::endl;
+        }
+        else
+        {
+            std::cout << "Area by sides: " << area_by_sides << std::endl;
+        }
+        
+    }
+    else if(n == 1)
+    {
+        // Пример 2: Если известно основание и высота
+        std::cout <<  "Enter base and height" << std::endl;
+        std::cin >> a >> h;
+        double area_by_base_and_height = calculate_area_by_base_and_height(a, h);
+        std::cout << "Area by base and height: " << area_by_base_and_height << std::endl;
+    }
     return 0;
 }
